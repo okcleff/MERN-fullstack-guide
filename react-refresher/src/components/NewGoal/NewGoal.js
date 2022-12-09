cda12af4143450bf2b54d8b320cfb5efb327893f
woncheolok <okcleff@gmail.com>
2022-12-09 20:53:05 +0900

    passing function via props

--- react-refresher/src/components/NewGoal/NewGoal.js
+++ react-refresher/src/components/NewGoal/NewGoal.js
@@ -2,22 +2,22 @@ import React from 'react';
 
 import './NewGoal.css';
 
-const NewGoal = () => {
-  const addGoalHandler = event => {
+const NewGoal = ({ onAddGoal }) => {
+  const addGoalHandler = (event) => {
     event.preventDefault();
 
     const newGoal = {
       id: Math.random().toString(),
-      text: 'My new goal!'
+      text: 'My new goal!',
     };
 
-    console.log(newGoal);
+    onAddGoal(newGoal);
   };
 
   return (
-    <form className="new-goal" onSubmit={addGoalHandler}>
-      <input type="text" />
-      <button type="submit">Add Goal</button>
+    <form className='new-goal' onSubmit={addGoalHandler}>
+      <input type='text' />
+      <button type='submit'>Add Goal</button>
     </form>
   );
 };
