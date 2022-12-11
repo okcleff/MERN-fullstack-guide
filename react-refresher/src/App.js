8b3799fadcc34c9d8e5121ef3158959b264dee80
woncheolok <okcleff@gmail.com>
2022-12-11 20:44:16 +0900

    working with states

--- react-refresher/src/App.js
+++ react-refresher/src/App.js
@@ -1,23 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import GoalList from './components/GoalList/GoalList';
 import NewGoal from './components/NewGoal/NewGoal';
 import './App.css';
 
 const App = () => {
-  const courseGoals = [
+  const [courseGoals, setCourseGoals] = useState([
     { id: 'cg1', text: 'Finish the Course' },
     { id: 'cg2', text: 'Learn all about the Course Main Topic' },
     { id: 'cg3', text: 'Help other students in the Course Q&A' },
-  ];
+  ]);
 
-  const addNewGoalHandler = (newGoal) => {
-    courseGoals.push(newGoal);
-    console.log(courseGoals);
+  const addNewGoalHandler = newGoal => {
+    setCourseGoals(courseGoals.concat(newGoal));
   };
 
   return (
-    <div className='course-goals'>
+    <div className="course-goals">
       <h2>Course Goals</h2>
       <NewGoal onAddGoal={addNewGoalHandler} />
       <GoalList goals={courseGoals} />
