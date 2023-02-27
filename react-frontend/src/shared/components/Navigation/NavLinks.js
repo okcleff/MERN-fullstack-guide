18c44e2c97b063bea6ff47f0a2a09ca2206f1d76
okcleff <okcleff@gmail.com>
2023-02-28 00:19:34 +0900

    Adding Authenticated & Unauthenticated Routes

--- react-frontend/src/shared/components/Navigation/NavLinks.js
+++ react-frontend/src/shared/components/Navigation/NavLinks.js
@@ -8,25 +8,30 @@ const NavLinks = (props) => {
   const auth = useContext(AuthContext);
 
   return (
-    <ul className='nav-links'>
+    <ul className="nav-links">
       <li>
-        <NavLink to='/' exact>
+        <NavLink to="/" exact>
           ALL USERS
         </NavLink>
       </li>
       {auth.isLoggedIn && (
         <li>
-          <NavLink to='/u1/places'>MY PLACES</NavLink>
+          <NavLink to="/u1/places">MY PLACES</NavLink>
         </li>
       )}
       {auth.isLoggedIn && (
         <li>
-          <NavLink to='/places/new'>ADD PLACE</NavLink>
+          <NavLink to="/places/new">ADD PLACE</NavLink>
         </li>
       )}
       {!auth.isLoggedIn && (
         <li>
-          <NavLink to='/auth'>AUTHENTICATE</NavLink>
+          <NavLink to="/auth">AUTHENTICATE</NavLink>
+        </li>
+      )}
+      {auth.isLoggedIn && (
+        <li>
+          <button onClick={auth.logout}>LOGOUT</button>
         </li>
       )}
     </ul>
