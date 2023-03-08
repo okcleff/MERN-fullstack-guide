6fdc28a320da2fb5af452b695bcd10a7b80779a2
okcleff <okcleff@gmail.com>
2023-03-08 13:51:41 +0900

    Adding Places - Specific Routes

--- node-backend/routes/places-routes.js
+++ node-backend/routes/places-routes.js
@@ -2,9 +2,24 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  console.log("GET Request in Places");
-  res.json({ message: "It works!" });
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "some description",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878531,
+    },
+    address: "20 W 34th St., New York, NY 10001 미국",
+    creator: "u1",
+  },
+];
+
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((p) => p.id === placeId);
+  res.json({ place });
 });
 
 module.exports = router;
