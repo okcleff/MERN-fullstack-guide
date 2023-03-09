c4c8a82314e25fc44559e1512654ea8361b3d5f3
okcleff <okcleff@gmail.com>
2023-03-09 12:21:01 +0900

    Adding Controllers

--- node-backend/routes/places-routes.js
+++ node-backend/routes/places-routes.js
@@ -1,45 +1,11 @@
 const express = require("express");
 
-const HttpError = require("../models/http-error");
+const placeControllers = require("../controllers/places-controllers");
 
 const router = express.Router();
 
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "some description",
-    location: {
-      lat: 40.7484405,
-      lng: -73.9878531,
-    },
-    address: "20 W 34th St., New York, NY 10001 미국",
-    creator: "u1",
-  },
-];
+router.get("/:pid", placeControllers.getPlaceById);
 
-router.get("/:pid", (req, res, next) => {
-  const placeId = req.params.pid;
-  const place = DUMMY_PLACES.find((p) => p.id === placeId);
-
-  if (!place) {
-    throw new HttpError("Could not find a place for the provided id.", 404);
-  }
-
-  res.json({ place });
-});
-
-router.get("/user/:uid", (req, res, next) => {
-  const userId = req.params.uid;
-  const place = DUMMY_PLACES.find((p) => p.creator === userId);
-
-  if (!place) {
-    return next(
-      new HttpError("Could not find a place for the provided user id.", 404)
-    );
-  }
-
-  res.json({ place });
-});
+router.get("/user/:uid", placeControllers.getPlaceByUserId);
 
 module.exports = router;
