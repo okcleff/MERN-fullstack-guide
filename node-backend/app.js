9cbaf87c988eb605833faeb8bb7bee9ec89e6735
okcleff <okcleff@gmail.com>
2023-03-10 12:15:26 +0900

    Validation API Inputs

--- node-backend/app.js
+++ node-backend/app.js
@@ -13,7 +13,7 @@ app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
 
 app.use((req, res, next) => {
-  const error = new HttpError("Could not find this route", 404);
+  const error = new HttpError("Could not find this route.", 404);
   throw error;
 });
 
@@ -23,7 +23,7 @@ app.use((error, req, res, next) => {
   }
 
   res.status(error.code || 500);
-  res.json({ message: error.message || "An unknown error occurred" });
+  res.json({ message: error.message || "An unknown error occurred." });
 });
 
 app.listen(5000);
