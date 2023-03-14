ce6db0f61b5459f5e752c46f61bc4282ccca0578
okcleff <okcleff@gmail.com>
2023-03-14 16:41:18 +0900

    Updating and Deleting Places

--- react-frontend/src/places/components/PlaceItem.js
+++ react-frontend/src/places/components/PlaceItem.js
@@ -4,10 +4,14 @@ import Card from '../../shared/components/UIElements/Card';
 import Button from '../../shared/components/FormElements/Button';
 import Modal from '../../shared/components/UIElements/Modal';
 import Map from '../../shared/components/UIElements/Map';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 import { AuthContext } from '../../shared/context/auth-context';
+import { useHttpClient } from '../../shared/hooks/http-hook';
 import './PlaceItem.css';
 
 const PlaceItem = (props) => {
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
   const auth = useContext(AuthContext);
   const [showMap, setShowMap] = useState(false);
   const [showConfirmModal, setShowConfirmModal] = useState(false);
@@ -24,13 +28,20 @@ const PlaceItem = (props) => {
     setShowConfirmModal(false);
   };
 
-  const confirmDeleteHandler = () => {
+  const confirmDeleteHandler = async () => {
     setShowConfirmModal(false);
-    console.log('DELETING...');
+    try {
+      await sendRequest(
+        `http://localhost:5000/api/places/${props.id}`,
+        'DELETE'
+      );
+      props.onDelete(props.id);
+    } catch (err) {}
   };
 
   return (
     <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
       <Modal
         show={showMap}
         onCancel={closeMapHandler}
@@ -43,6 +54,7 @@ const PlaceItem = (props) => {
           <Map center={props.coordinates} zoom={16} />
         </div>
       </Modal>
+
       <Modal
         show={showConfirmModal}
         onCancel={cancelDeleteHandler}
@@ -64,25 +76,31 @@ const PlaceItem = (props) => {
           can't be undone thereafter.
         </p>
       </Modal>
+
       <li className="place-item">
         <Card className="place-item__content">
+          {isLoading && <LoadingSpinner asOverlay />}
+
           <div className="place-item__image">
             <img src={props.image} alt={props.title} />
           </div>
+
           <div className="place-item__info">
             <h2>{props.title}</h2>
             <h3>{props.address}</h3>
             <p>{props.description}</p>
           </div>
+
           <div className="place-item__actions">
             <Button inverse onClick={openMapHandler}>
               VIEW ON MAP
             </Button>
-            {auth.isLoggedIn && (
+
+            {auth.userId === props.creatorId && (
               <Button to={`/places/${props.id}`}>EDIT</Button>
             )}
 
-            {auth.isLoggedIn && (
+            {auth.userId === props.creatorId && (
               <Button danger onClick={showDeleteWarningHandler}>
                 DELETE
               </Button>
--- react-frontend/src/places/components/PlaceList.js
+++ react-frontend/src/places/components/PlaceList.js
@@ -8,27 +8,28 @@ import './PlaceList.css';
 const PlaceList = (props) => {
   if (props.items.length === 0) {
     return (
-      <div className='place-list center'>
+      <div className="place-list center">
         <Card>
           <h2>No places found. Maybe create one?</h2>
-          <Button to='/places/new'>Share Place</Button>
+          <Button to="/places/new">Share Place</Button>
         </Card>
       </div>
     );
   }
 
   return (
-    <ul className='place-list'>
+    <ul className="place-list">
       {props.items.map((place) => (
         <PlaceItem
           key={place.id}
           id={place.id}
-          image={place.imageUrl}
+          image={place.image}
           title={place.title}
           description={place.description}
           address={place.address}
           creatorId={place.creator}
           coordinates={place.location}
+          onDelete={props.onDeletePlace}
         />
       ))}
     </ul>
