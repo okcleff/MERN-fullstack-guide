cf798c18b6b4c6ef541de31fb3e15eebf61a36f9
okcleff <okcleff@gmail.com>
2023-03-15 10:19:36 +0900

    Uploading and Serving Images Statically

--- node-backend/controllers/users-controllers.js
+++ node-backend/controllers/users-controllers.js
@@ -50,8 +50,7 @@ const signup = async (req, res, next) => {
   const createdUser = new User({
     name,
     email,
-    image:
-      "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80",
+    image: req.file.path,
     password,
     places: [],
   });
